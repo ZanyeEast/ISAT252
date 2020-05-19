570a3e618bb26009f12ebe3f9c3005e9c4b44535
EC2 Default User <ec2-user@ip-172-31-189-209.ec2.internal>
2020-05-19 15:20:51 +0000

    added my lab2.py file.

---
+++ lab2.py
@@ -0,0 +1,39 @@
+"""
+Lab 2 Basic Data Types
+"""
+
+# Defining string and printing string
+my_name = "Tom"
+print(my_name.upper())
+# Defining int and printing int
+my_id = your_id = 123
+print(my_id)
+print(your_id)
+"""
+123 can not be identified as a variable due to the fact that
+integers and floats can not be defined as variables
+"""
+# Defining and printing my_id as a string
+my_id_str = "123"
+print(my_id_str)
+
+"""
+You can not perform my_name + my_id due to the fact that
+you can not add a string and an integers
+"""
+# Printing my_name + my_id_str
+print(my_name + my_id_str)
+print(my_name*3)
+
+#Splitting string
+print("hello, world. This is my first python string.".split("."))
+
+#Defining message variable
+"""
+message variable can not be defined by using the single
+quotation marks due to the apostorphe in Tom's. Although
+the message can be printed using the double quotation marks
+(seen below)
+"""
+message = "Tom's id is 123"
+print(message)
---
+++ lec2.py
@@ -0,0 +1,29 @@
+"""
+this is my second py code
+
+for my 2nd lecture
+"""
+
+
+#print('hello world') # this is a single line comment
+
+# this is my 2nd line comment
+
+#print(type('123.'))
+
+#print("Hello World".upper())
+#print("Hello World".lower())
+
+#print("hello " + "world" + ".")
+"""
+my_str = "hello world"
+print(my_str)
+
+my_str = "Tom"
+print(my_str)
+"""
+my_int = 2
+
+my_float = 3.0
+
+print(my_int + my_float)
